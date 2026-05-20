97afe400546a3febc990e84c7ae4a1c6a6439e8c
Kien-civic <157670027+Kien-civic@users.noreply.github.com>
2026-05-20 10:12:39 +0700

    Update LevelSelect.cs

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -10,41 +10,41 @@ public class LevelSelect : MonoBehaviour
 
     public void LoadLevel2()
     {
-        SceneManager.LoadScene("Level2");
+        SceneManager.LoadScene("Tang2");
     }
 
     public void LoadLevel3()
     {
-        SceneManager.LoadScene("Level3");
+        SceneManager.LoadScene("Tang3");
     }
 
     public void LoadLevel4()
     {
-        SceneManager.LoadScene("Level4");
+        SceneManager.LoadScene("Tang4");
     }
 
     public void LoadLevel5()
     {
-        SceneManager.LoadScene("Level5");
+        SceneManager.LoadScene("Tang5");
     }
 
     public void LoadLevel6()
     {
-        SceneManager.LoadScene("Level6");
+        SceneManager.LoadScene("Tang6");
     }
 
     public void LoadLevel7()
     {
-        SceneManager.LoadScene("Level7");
+        SceneManager.LoadScene("Tang7");
     }
 
     public void LoadLevel8()
     {
-        SceneManager.LoadScene("Level8");
+        SceneManager.LoadScene("Tang8");
     }
 
     public void LoadLevel9()
     {
-        SceneManager.LoadScene("Level9");
+        SceneManager.LoadScene("Tang9");
     }
 }
