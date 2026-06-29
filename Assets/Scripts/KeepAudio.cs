b1ce61f354aecca5a99c61b339e7a73074c9d38b
Kien-civic <157670027+Kien-civic@users.noreply.github.com>
2026-06-29 15:44:42 +0700

    Add files via upload

---
+++ Assets/Scripts/KeepAudio.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class KeepAudio : MonoBehaviour
+{
+    // Tạo một biến static để quản lý độc nhất (Singleton Pattern)
+    private static KeepAudio instance;
+
+    void Awake()
+    {
+        // Kiểm tra xem đã có một bản sao của AudioManager nào tồn tại chưa
+        if (instance == null)
+        {
+            instance = this;
+
+            // LỆNH QUAN TRỌNG: Giữ đối tượng này không bị xóa khi chuyển Scene
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            // Nếu quay lại MainMenu mà đã có nhạc đang chạy, xóa ngay bản sao mới tạo để tránh trùng nhạc
+            Destroy(gameObject);
+        }
+    }
+}
