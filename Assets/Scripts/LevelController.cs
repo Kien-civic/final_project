aa2a0325930939ece5f7e945c2a9e1fcf6c20443
Kien-civic <157670027+Kien-civic@users.noreply.github.com>
2026-07-27 21:04:16 +0700

    Add files via upload

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class LevelController : MonoBehaviour
+{
+    // This function will be called when you click the Next Level button.
+    public void LoadNextLevel()
+    {
+        // Get the index of the current scene
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        // Calculate the index of the next scene    
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        // Check if there is a next level in the build settings
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            // If there is, load the next level
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            // If it's the last level, return to the main menu (Scene index 0)
+            Debug.Log("Đã hết Level! Quay lại Menu chính.");
+            SceneManager.LoadScene(0);
+        }
+    }
+}
